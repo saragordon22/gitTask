8fba66fa09318afd3faa2157d12d439b0d3b9efb
Sara Gordon <sara_pallotta@hotmail.co.uk>
2022-09-15 10:14:07 +0100

    changed alert message with user input

--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,6 @@
-alert("Git is Awesome!");
+function myFunction() {
+    let person = prompt("Please enter your name");
+    if (person != null) {
+      alert("Hello " + person + "! How are you today?");
+    }
+  }
